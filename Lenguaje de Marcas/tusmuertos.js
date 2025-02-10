d2ecde6e4efb2f2000f7b443fdeed041b2b524c7
thatpersianass <marcosjavierpgomez@gmail.com>
2025-02-10 14:00:16 +0000

    cock it forward

--- Lenguaje de Marcas/tusmuertos.js
+++ Lenguaje de Marcas/tusmuertos.js
@@ -1,6 +1,18 @@
-let a = document.getElementById('nombre').value
+// function mostrarPoia() {
+//     document.getElementById('texto').innerHTML = 'Adios mundo cruel';
+// }
 
-document.getElementById('nombre').value = a;
+let coches = ['seat', 'bmw', 'ford', 'byd'];
+let contador = 0;
 
-document.getElementById('nombre').innerHTML
+document.getElementById('texto').innerHTML = (coches[contador]);
 
+function mostrarPoia() {
+    contador++; // Aumentar el contador
+
+    if (contador >= coches.length) {
+        contador = 0; // Reiniciar el contador si se excede
+    }
+
+    document.getElementById('texto').innerHTML = coches[contador];
+}
