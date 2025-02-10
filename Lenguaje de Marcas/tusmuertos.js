8e9060e490caeca554c5b8cf75b28092dbfdd878
thatpersianass <marcosjavierpgomez@gmail.com>
2025-02-10 13:29:05 +0000

    cock it back

--- Lenguaje de Marcas/tusmuertos.js
+++ Lenguaje de Marcas/tusmuertos.js
@@ -1,18 +1,6 @@
-function validarFormulario(event) {
-    let email = document.getElementById("email").value;
-    let telefono = document.getElementById("telefono").value;
-    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    let telefonoRegex = /^\d{9,15}$/; // Acepta entre 9 y 15 dígitos
-    
-    if (!telefonoRegex.test(telefono)) {
-        alert("Por favor, ingrese un número de teléfono válido (9-15 dígitos).");
-        return;
-    }
-    
-    if (email !== "" && !emailRegex.test(email)) {
-        alert("Por favor, ingrese un correo electrónico válido.");
-        return;
-    }
-    
-    document.getElementById("formulario").submit();
-}
+let a = document.getElementById('nombre').value
+
+document.getElementById('nombre').value = a;
+
+document.getElementById('nombre').innerHTML
+
