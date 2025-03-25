fa1b74d73ee058612c549ec8de8c8a3c676ab35c
thatpersianass <marcosjavierpgomez@gmail.com>
2025-03-25 12:16:44 +0000

    gogogogogo

---
+++ English/ex.js
@@ -0,0 +1,31 @@
+// EX 5
+console.log("a. allocate");
+console.log("b. step-by-step");
+console.log("c. requirements");
+console.log("d. run");
+console.log("e. partition");
+console.log("f. component");
+
+// EX 6
+console.log("1. pre-installed");
+console.log("2. crash");
+console.log("3. BIOS");
+console.log("4. bit");
+console.log("5. prerequisite");
+
+// EX 7
+console.log("1. patch");
+console.log("2. screen resolution"),
+console.log("3. distro");
+console.log("4. segment");
+console.log("5. kernel");
+console.log("6. infrastructure");
+console.log("7. environment");
+
+// EX 8
+console.log("1. can have a value of 0 or 1");
+console.log("2. to fix a software problem");
+console.log("3. if u want multiple systems on ur computer");
+console.log("4. if the picture on the monitor isn't clear");
+console.log("5. many basic functions of the operating system");
+console.log("6. if u want to install something correctly");
